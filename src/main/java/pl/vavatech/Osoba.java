d8941b0b177ca0a3adb58e9dcb06b010b5807bdc
Piotr Przybylak <piotr.przybylak@gmail.com>
2016-10-26 15:21:07 +0200

    Sprawdzenie czyJestWysoka w zaleznosci od Plci

--- src/main/java/pl/vavatech/Osoba.java
+++ src/main/java/pl/vavatech/Osoba.java
@@ -5,8 +5,13 @@ public class Osoba {
     private String imie, nazwisko;
     private int wiek;
     private int waga;
+    private boolean czyJestMężczyzną;
+    private int wzrost;
 
-    public Osoba(String imie, String nazwisko, int wiek, int waga) {
+
+    public Osoba(String imie, String nazwisko, int wiek, int waga, boolean czyJestMężczyzną, int wzrost) {
+        this.czyJestMężczyzną = czyJestMężczyzną;
+        this.wzrost = wzrost;
         if (imie != null) {
             this.imie = imie;
         } else {
@@ -31,5 +36,15 @@ public class Osoba {
     int podajWagę() {
         return waga;
     }
+
+    public boolean czyJestWysoka() {
+        if(czyJestMężczyzną && wzrost >= 180 || !czyJestMężczyzną && wzrost >= 150) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+
 }
 
--- src/main/java/pl/vavatech/OsobaMain.java
+++ src/main/java/pl/vavatech/OsobaMain.java
@@ -5,12 +5,12 @@ import java.util.ArrayList;
 public class OsobaMain {
 
     public static void main(String[] args) {
-        Osoba tato = new Osoba("Jan", "Kowalski", 35, 150);
-        Osoba mama = new Osoba("Barbara", "Kowalska", 35, 40);
-        Osoba corka = new Osoba("Alicja",  "Kowalska", 10, 20);
-        Osoba syn = new Osoba("Stefan", "Kowalski", 10, 120);
+        Osoba tato = new Osoba("Jan", "Kowalski", 35, 150, true, 170);
+        Osoba mama = new Osoba("Barbara", "Kowalska", 35, 40, false, 170);
+        Osoba corka = new Osoba("Alicja", "Kowalska", 10, 20, false, 170);
+        Osoba syn = new Osoba("Stefan", "Kowalski", 10, 120, true, 170);
 
-        Osoba nn = new Osoba(null, null, 0, 50);
+        Osoba nn = new Osoba(null, 0, 50);
 
         System.out.println(tato.pelnoletnia()); // true
         System.out.println(corka.pelnoletnia()); // false
@@ -22,22 +22,27 @@ public class OsobaMain {
 
 
         Rodzina kowalscy;
+
+
+        kowalscy = new Rodzina(mama, tato, dzieci);
+
+        //  Rodzina nowakowie = new Rodzina();
+
+        int łacznaWagaRodziny = kowalscy.lacznaWaga();
+        System.out.println("Rodzina wsiada do windy, obciążenie: " + łacznaWagaRodziny);
+
+        final int wagaKowalskich = ParkRozrywki.lacznaWaga(kowalscy);
+
+        System.out.println("Rodzina wsiada na karuzelę, obciążenie: " + wagaKowalskich);
+
+        System.out.println("Czy mama jest wyskoka: " + mama.czyJestWysoka());
+
+        System.out.println();
+
         try {
-
-            kowalscy = new Rodzina(mama, tato, dzieci);
-
-            //  Rodzina nowakowie = new Rodzina();
-
-
-            int łacznaWagaRodziny = kowalscy.lacznaWaga();
-            System.out.println("Rodzina wsiada do windy, obciążenie: " + łacznaWagaRodziny);
-
-            final int wagaKowalskich = ParkRozrywki.lacznaWaga(kowalscy);
-
-            System.out.println("Rodzina wsiada na karuzelę, obciążenie: " + wagaKowalskich);
-
+            new Rodzina(null, tato, dzieci);
         } catch (NullPointerException exception) {
-
+            System.out.println("OOOOO : "+ exception);
         }
     }
 
